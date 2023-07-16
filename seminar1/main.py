f55b7982d7093af1002eae5d9e94bfdecfa37eb6
«Vitaliy.K <dr.kovalyk2017@yandex.ru>
2023-07-16 22:38:35 +0300

    seminar1

---
+++ seminar1/main.py
@@ -0,0 +1,21 @@
+
+# Задача про классы и парты
+
+# class1 = int(input("Введите кол-во учащихся в 1ом классе: "))
+# class2 = int(input("Введите кол-во учащихся во 2ом классе: "))
+# class3 = int(input("Введите кол-во учащихся в 3ем классе: "))
+
+# parti = (class1 + class2 + class3 + 1) // 2
+# print(parti)
+
+#  високосность(год : 4 и не : 100 или годо : 400)
+
+# ears = int(input("Введите год: "))
+# if (ears % 4 == 0) and (ears % 100 != 0):
+#     print("YES")
+# elif ears % 400 == 0:
+#     print("YES")
+# else:
+#     print("NO")        
+ 
+
