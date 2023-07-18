9712dedb658943abe0795a074b88d78282fd1877
«Vitaliy.K <dr.kovalyk2017@yandex.ru>
2023-07-18 22:12:40 +0300

    sem1

--- seminar1/main.py
+++ seminar1/main.py
@@ -19,3 +19,39 @@
 #     print("NO")        
  
 
+# n = int(input("Введите число: "))
+# sum = 0
+# while (n != 0):
+#     sum += n % 10
+#     n = n // 10
+# print(sum)    
+
+# n = 6
+
+# a = (n // 3) / 2
+# b = (n // 3) / 2
+# c = (a + b) * 2
+# print(f"({int(a)},{int(c)},{int(b)})")
+
+# n = 375916
+# a = n // 1000
+# b = n % 1000
+# sum1 = 0
+# sum2 = 0
+# while a != 0 and b != 0:
+#     sum1 += a % 10
+#     sum2 += b % 10
+#     a //= 10
+#     b //=10
+# if sum1 == sum2:
+#     print("yes")
+# else:
+#     print("no")   
+
+# a = 3
+# b = 2
+# c = 4
+# if ((c > a) or (c > b)) and c <= (a * b):
+#     print("YES")
+# else:
+#     print("NO")     
