d223b82342f0376a3eec3811b633561575ac8bf6
«Vitaliy.K <dr.kovalyk2017@yandex.ru>
2023-08-27 17:07:49 +0300

    sem8

--- seminar8/algoritm.py
+++ seminar8/algoritm.py
@@ -1,10 +1,11 @@
-from interaeys import *
+from interfeys import *
 
+start = 0
 
 def menu_func(start):
     if start == 1:
         from work_file import add_contact
-        from interaeys import stroka
+        from interfeys import stroka
         strok = stroka()
         add_contact('tel_book.txt', strok)
     elif start == 2:
@@ -12,12 +13,23 @@ def menu_func(start):
         print_conatact('tel_book.txt')
     elif start == 3:
         from work_file import search_username
-        from interaeys import name
+        from interfeys import name
         nam = name()
         search_username('tel_book.txt', nam)
+    elif start == 4:
+        from work_file import replace_name
+        from interfeys import new_name, old_name
+        old_nam = old_name()
+        new_nam = new_name()
+        replace_name('tel_book.txt', old_nam, new_nam)
+    elif start == 5:
+        from work_file import delete_name
+        from interfeys import deleted_name
+        del_name = deleted_name()
+        delete_name('tel_book.txt', del_name)    
 
-start = 0
-while start != 4:
+
+while start != 6:
     print()
     start = print_menu()
     menu_func(start)
--- seminar8/work_file.py
+++ seminar8/work_file.py
@@ -1,25 +1,44 @@
 
 def add_contact(file_name, spisok):
-    tel = open(file_name, 'a')
+    tel = open(file_name, 'a', encoding='utf-8')
     tel.write(spisok + '\n')
     tel.close
 
 def print_conatact(file_name):
-    tel = open(file_name, 'r')
+    tel = open(file_name, 'r', encoding='utf-8')
     for line in tel:
         print(line)
     tel.close
 
 def search_username(file_name, name):
     flag = False
-    tel = open(file_name, 'r')
+    tel = open(file_name, 'r', encoding='utf-8')
     for line in tel:
         if name in line:
             print(line)
             flag = True
     if flag == False:
         print("не найдено")           
-    tel.close        
+    tel.close
+
+def replace_name(file_name, old_name, new_name):
+    with open(file_name, 'r+', encoding='utf-8') as tel:
+        lines = tel.readlines()
+        with open(file_name, 'w+', encoding='utf-8') as tel2:
+            for line in lines:
+                if old_name in line:
+                    line = line.replace(old_name, new_name)
+                tel2.write(line)
+        print("Данные успешно изменены")
+
+def delete_name(file_name, delete_name):
+    with open(file_name, 'r', encoding='utf-8') as tel:
+        lines = tel.readlines()
+        with open(file_name, 'w', encoding='utf-8') as tel2:
+            for line in lines:
+                if delete_name not in line:
+                    tel2.write(line)
+            print("Контакт удалён!")
 
 
 
